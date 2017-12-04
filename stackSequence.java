76833546b0c7264d733fd7f357130a6ea46f6f36
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-04 17:03:22 +0900

    스택수열(1874번)
    아직 완벽하게 처리하지 못함.
    틀린케이스를 못찾음.

---
+++ stackSequence.java
@@ -0,0 +1,48 @@
+package backjoon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//1874
+public class stackSequence {
+	public static void main(String[] args){
+		ArrayList<Integer> list = new ArrayList<Integer>(); //최초 list
+		ArrayList<Integer> list2 = new ArrayList<Integer>(); //가능한지 확인하는 list
+		ArrayList<String> listTotal = new ArrayList<String>();
+		Scanner sc = new Scanner(System.in);
+		boolean success = true;
+		int n = sc.nextInt();
+		int i, j, k=0;
+		for(i=0; i<n; i++){
+			list.add(sc.nextInt());
+		}
+		int value=1;
+		for(j=0; j<n*2; j++){
+			if(list2.isEmpty() == true){
+				list2.add(value++);
+				listTotal.add("+");
+			}else{
+				if(list2.get(list2.size()-1) < list.get(k)){
+					list2.add(value++);
+					listTotal.add("+");
+				}else if(list2.get(list2.size()-1) == list.get(k)){
+//					System.out.println("list2.get(list2.size()-1: "+list2.get(list2.size()-1));
+					list2.remove(list2.size()-1);
+					k++;
+					listTotal.add("-");
+				}else{
+					success = false;
+					break;
+				}
+			}
+		}
+//		System.out.println("listTotal.size(): "+listTotal.size());
+		if(success == true && list2.isEmpty() == true && value == n+1 && j == n*2){
+			for(i=0; i<n*2; i++){
+				System.out.println(listTotal.get(i));
+			}
+		}else{
+			System.out.println("NO");
+		}
+	}
+}
