d1c0cae19987133304a3344f104c2f6e093b7215
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-04 17:26:51 +0900

    로프 (2217)
    그리디 알고리즘

---
+++ rope.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//2217
+public class rope {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int max=0;
+		int n = sc.nextInt();
+		int i;
+		int arr[] = new int[n];
+		
+		for(i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		for(i=0; i<n; i++){
+			if(max < arr[i]*(n-i)){
+				max = arr[i]*(n-i);
+			}
+		}
+		System.out.println(max);
+	}
+}
