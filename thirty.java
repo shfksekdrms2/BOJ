3a1af790273534fe569e29564904103b1ce0bfe8
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-04 19:39:15 +0900

    30 (10610)
    그리디 알고리즘
    (자릿수가 10^5 자리라 String 형식으로 받고 int 배열에 한자리씩 삽입)

---
+++ thirty.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//10610
+public class thirty {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int arr[] = new int[str.length()];
+		int i;
+		int sum=0;
+		for(i=0; i<arr.length; i++){
+			arr[i] = str.charAt(i)-48;
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+		if(sum%3 == 0 && arr[0] == 0){
+			for(i=arr.length-1; i>=0; i--){
+				System.out.print(arr[i]);
+			}
+		}else{
+			System.out.println("-1");
+		}
+	}
+}
