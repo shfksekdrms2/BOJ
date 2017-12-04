dc45f08ab1d77ce5c30f06a25188bb4ec0a355dd
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-04 20:43:06 +0900

    리팩토링 by 영준
    MySort 클래스가 아닌 String 클래스를 바로 넘겨 처리할 수 있도록 수정

--- wordSort.java
+++ wordSort.java
@@ -1,56 +1,39 @@
 package backjoon;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-//1181
 public class wordSort {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		ArrayList<MySort> list = new ArrayList<MySort>();
+		String[] str = new String[n];
 		int i;
-		MySort sort;
-		String input = "";
+		MySort sort = new MySort();
 		for(i=0; i<n; i++){
-			sort = new MySort();
-			input = sc.next();
-			sort.setWord(input);
-			list.add(sort);
+			str[i] = sc.next();
 		}
-		Collections.sort(list, new MySort());
+		Arrays.sort(str, sort);
 		
-		System.out.println(list.get(0).getWord());
-		for(i=1; i<list.size(); i++){
-			if(!list.get(i).getWord().equals(list.get(i-1).getWord())){
-				System.out.println(list.get(i).getWord());
+		System.out.println(str[0]);
+		for(i=1; i<str.length; i++){
+			if(!str[i].equals(str[i-1])){
+				System.out.println(str[i]);
 			}
 		}
 	}
-	public static class MySort implements Comparator<MySort>{
-		private String word;
+	public static class MySort implements Comparator<String>{
 		
-		public String getWord() {
-			return word;
-		}
-
-		public void setWord(String word) {
-			this.word = word;
-		}
-
 		@Override
-		public int compare(MySort o1, MySort o2) {
-			String first = o1.getWord();
-			String second = o2.getWord();
-			if(first.length() < second.length()){
+		public int compare(String o1, String o2) {
+			if(o1.length() < o2.length()){
 				return -1;
-			}else if(first.length() > second.length()){
+			}else if(o1.length() > o2.length()){
 				return 1;
 			}else{
-				char[] firstChar = first.toCharArray();
-				char[] secondChar = second.toCharArray();
+				char[] firstChar = o1.toCharArray();
+				char[] secondChar = o2.toCharArray();
 				int i;
 				for(i=0; i<firstChar.length; i++){
 					if(firstChar[i] < secondChar[i]){
