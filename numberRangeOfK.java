fedba1b5bc0240bf3ad62a45f0e73ccdb3636e51
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-07 00:08:14 +0900

    K 번쨰 숫자 (7469)
    - Collection.sort(lsit)형식에서 Arrays.sort(배열) 형식으로 변경하여 시간 초과 문제 해결

--- numberRangeOfK.java
+++ numberRangeOfK.java
@@ -1,65 +1,51 @@
-package backjoon;
+package backjoon.BOJ;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 //7469
 public class numberRangeOfK {
-	static class tree{
-		private int x, y;
-		tree(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-		public int getX() {
-			return x;
-		}
-		public int getY() {
-			return y;
-		}
-	}
-	static class treeSort implements Comparator<tree>{
+	static class mySort implements Comparator<int []>{
 		@Override
-		public int compare(tree o1, tree o2) {
+		public int compare(int[] o1, int[] o2) {
 			// TODO Auto-generated method stub
-			if(o1.getX() > o2.getX()){
+			if(o1[0] > o2[0]){
 				return 1;
-			}else if(o1.getX() < o2.getX()){
+			}else if(o1[0] < o2[0]){
 				return -1;
 			}else{
 				return 0;
 			}
 		}
 	}
-	static LinkedList<tree> list;
 	static int n;
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		int m = sc.nextInt();
-		list = new LinkedList<>();
 		int i;
+		int[][] arr = new int[n][2];
 		for(i=0; i<n; i++){
-			list.add(new tree(sc.nextInt(), i+1));
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = i+1;
 		}
-		Collections.sort(list, new treeSort());
+		Arrays.sort(arr, new mySort());
 		int a,b,k;
 		for(i=0; i<m; i++){
 			a = sc.nextInt();
 			b = sc.nextInt();
 			k = sc.nextInt();
-			Q(a, b, k);
+			Q(a, b, k, arr);
 		}
 	}
-	static void Q(int i, int j, int k){
+	static void Q(int i, int j, int k, int[][] arr){
 		int cnt=0, a;
 		for(a=0; a<n; a++){
-			if(list.get(a).getY() >= i && list.get(a).getY() <= j){
+			if(arr[a][1] >= i && arr[a][1] <= j){
 				cnt++;
 				if(cnt == k){
-					System.out.println(list.get(a).getX());
+					System.out.println(arr[a][0]);
 					break;
 				}
 			}
