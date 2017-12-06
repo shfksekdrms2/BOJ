a3bc7a00ab4131b202a26cf09fc611e7c3cab1b6
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-06 23:59:40 +0900

    좌표 정렬 (11650)
    - list로 만들어서 Collection 정렬할 경우에 시간 초과 발생
    -  배열로 만들어서 Arrays.sort로 정렬
    - sort 호출시 comparator에서  2차 배열의 경우 1차 배열로 1차 배열의 경우 해당 형식으로 넘겨줘야 한다.

---
+++ coordinatesSort.java
@@ -0,0 +1,46 @@
+package backjoon.BOJ;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+//11650
+public class coordinatesSort {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i;
+		int x, y;
+		int[][] arr = new int[n][2];
+		for(i=0; i<n; i++){
+			x = sc.nextInt();
+			y = sc.nextInt();
+			arr[i][0] = x;
+			arr[i][1] = y;
+		}
+		Arrays.sort(arr, new mySort());
+		for(i=0; i<n; i++){
+			System.out.println(arr[i][0] +" " + arr[i][1]);
+		}
+	}
+	
+	static class mySort implements Comparator<int []>{
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			// TODO Auto-generated method stub
+			if(o1[0] > o2[0]){
+				return 1;
+			}else if(o1[0] < o2[0]){
+				return -1;
+			}else{
+				if(o1[1] > o2[1]){
+					return 1;
+				}else if(o1[1] < o2[1]){
+					return -1;
+				}else{
+					return 0;
+				}
+			}
+		}
+	}
+}
