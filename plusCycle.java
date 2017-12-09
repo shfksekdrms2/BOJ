4b46cc388d867c1f60673cb8928c49ebc468e89a
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-09 23:55:38 +0900

    더하기 사이클 (1110)
    그리디 알고리즘

---
+++ plusCycle.java
@@ -0,0 +1,30 @@
+package backjoon.BOJ;
+
+import java.util.Scanner;
+
+//1110
+public class plusCycle {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int value = input;
+		int unit;
+		int sum;
+		int cnt=0;
+		if(input < 10){
+			System.out.println("1");
+		}else{
+			while(true){
+				cnt++;
+				sum = (value/10) + (value%10);
+				unit = (value%10)*10;
+				unit += sum%10;
+				value = unit;
+				if(input == value){
+					break;
+				}
+			}
+			System.out.println(cnt);
+		}
+	}
+}
