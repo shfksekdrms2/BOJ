5db1d2ad5407fcf6aaecccf68ff98d5e6efb97ac
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-13 01:56:26 +0900

    카이사르 암호 (5598)
    - 문자열 처리
    -  암호화 알고리즘( 원래 문자 -3 으로 이동)

---
+++ GaiusJuliusCaesarEncryption.java
@@ -0,0 +1,23 @@
+package backjoon.BOJ;
+
+import java.util.Scanner;
+
+//5598
+public class GaiusJuliusCaesarEncryption {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		char[] arr = input.toCharArray();
+		int i;
+		for(i=0; i<input.length(); i++){
+//			System.out.println("(int)arr[i]: "+ (int)arr[i]);
+//			System.out.println("arr[i]: "+ arr[i]);
+			if((int)arr[i]-3-65 < 0){
+//				System.out.println("(int)arr[i]-3-65 : "+((int)arr[i]-3-65));
+				System.out.print((char)(arr[i]-3+26));
+			}else{
+				System.out.print((char)(arr[i]-3));
+			}
+		}
+	}
+}
