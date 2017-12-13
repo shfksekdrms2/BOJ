5ab46667df366ff7a1f61858008e29d2ffc49963
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-14 08:03:28 +0900

    문자열 폭발 (9935)
    input type 변경(String -> char[])
    --> 여전히 시간 초과 발생...

--- stringExplosion.java
+++ stringExplosion.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class stringExplosion {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
+		char[] input = sc.next().toCharArray();
 		String boomStr = sc.next();
 		int i, j=0;
+		char[] result = new char[input.length];
 		
-		char[] result = new char[input.length()];
-		for(i=1; i<=input.length(); i++){
-			result[j++] = input.substring(i-1, i).charAt(0);
+		for(i=1; i<=input.length; i++){
+			result[j++] = input[i-1];
 			if(j-boomStr.length() >= 0 && String.valueOf(result).substring(j-boomStr.length(), j).equals(boomStr)){
 				j -= boomStr.length();
 			}
-			if(i == input.length()){
+			if(i == input.length){
 				for(int k=0; k<i-j; k++){
 					result[j+k] = '\0';
 				}
