163948e80538b14bca34bae00fa5e1e06dbed70c
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-16 08:02:30 +0900

    단어 길이 재기 (2743)
    문자열 처리

---
+++ wordLengthMeasurement.java
@@ -0,0 +1,12 @@
+package backjoon.BOJ;
+
+import java.util.Scanner;
+
+//2743
+public class wordLengthMeasurement {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		System.out.println(input.length());
+	}
+}
