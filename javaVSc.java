1001874f0aea30b388e52b0ecbfd51af1e266c8b
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-17 15:03:39 +0900

    java vs c++ (3613)
    58% 에서 틀림
    예외 케이스 추가.

--- javaVSc.java
+++ javaVSc.java
@@ -13,10 +13,16 @@ public class javaVSc {
 		int cnt[] = new int[2];
 		for(i=0; i<input.length; i++){
 			if(input[i] == '_'){
-				i++;
-				input[i] -= 32; 
-				error = false;
-				cnt[0] = 1;
+				//연속으로 _ 가 들어 왔을경우에 대한 예외
+				if(i+1 < input.length){
+					i++;
+					input[i] -= 32; 
+					error = false;
+					cnt[0] = 1;
+				}else{
+					error = true;
+					break;
+				}
 			}else if(input[i] >= 'A' && input[i] <= 'Z'){
 				result += "_";
 				input[i] += 32;
@@ -25,20 +31,26 @@ public class javaVSc {
 			}
 			result += input[i];
 		}
+		//혼합의 경우 체크
 		if(cnt[0] + cnt[1] == 2){
 			error = true;
 		}
 		if(error == false){
-			if(result.substring(0, 1).equals("_") || result.substring(result.length()-1, result.length()).equals("_") 
-					|| (result.substring(0, 1).charAt(0) >= 'A' && result.substring(0, 1).charAt(0) <= 'Z')){
+			//처음이  _ 인 경우
+			//처음이 대문자로 시작하는 경우
+			if(result.substring(0, 1).equals("_") || 
+					(result.substring(0, 1).charAt(0) >= 'A' && result.substring(0, 1).charAt(0) <= 'Z')){
 				System.out.println("Error!");
 			}else{
+				//문자가 소/대문자, _ 인 경우인지 체크
 				for(i=0; i<result.length(); i++){
 					if((result.substring(i, i+1).charAt(0) >= 'A' && result.substring(i, i+1).charAt(0) <= 'Z') ||
-							result.substring(i, i+1).charAt(0) >= 'a' && result.substring(i, i+1).charAt(0) <= 'z'){
+							result.substring(i, i+1).charAt(0) >= 'a' && result.substring(i, i+1).charAt(0) <= 'z' ||
+							result.substring(i, i+1).charAt(0) >= '_'){
 						error = false;
 					}else{
 						error = true;
+						break;
 					}
 				}
 				if(error == false){
