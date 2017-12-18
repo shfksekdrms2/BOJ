486ecc9622915bda680c5d114330006c47a68f93
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-18 22:01:22 +0900

    애너그램 만들기 (1919)
    문자열 처리

---
+++ anagram.java
@@ -0,0 +1,27 @@
+package backjoon.BOJ;
+
+import java.util.Scanner;
+
+//1919
+public class anagram {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		char[] input1 = sc.next().toCharArray();
+		char[] input2 = sc.next().toCharArray();
+		int[][] cnt = new int[26][2];
+		int i;
+		for(i=0; i<input1.length; i++){
+			cnt[input1[i]-97][0]++;
+		}
+		for(i=0; i<input2.length; i++){
+			cnt[input2[i]-97][1]++;
+		}
+		int result=0;
+		for(i=0; i<cnt.length; i++){
+			if(cnt[i][0] != cnt[i][1]){
+				result += Math.abs(cnt[i][0]-cnt[i][1]);
+			}
+		}
+		System.out.println(result);
+	}
+}
