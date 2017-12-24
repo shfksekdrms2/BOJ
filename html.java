9ca20de692f68866491f9472575d0e4c88ece862
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-24 19:51:53 +0900

    HTML (6581)
    다 이어 붙이고 token 으로 이후 문자열의 길이가 80이 넘는지 확인해야함

--- html.java
+++ html.java
@@ -8,23 +8,37 @@ public class html {
 	static String hr = "<hr>";
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		String input;
+//		String input;
+		String[] inputArr;
 		String output="";
-		int i, j;
-		while(sc.hasNextLine()){
-			input = sc.nextLine();
-			for(i=0; i<input.length()-4; i++){
-				if(input.substring(i, i+4).equals(br)){
+		int i, j, k=0;
+//		while(sc.hasNextLine()){
+		for(j=0; j<12; j++){
+//			input = sc.nextLine();
+			inputArr = sc.nextLine().split("");			
+			for(i=0; i<inputArr.length; i++){
+				if(inputArr[i].equals(br)){
 					output += "\n";
-					i=i+3;
-				}else if(input.substring(i, i+4).equals(hr)){
-					output += "--------------------------------------------------------------------------------";
-					i=i+3;
-				}else{
-					output += input.substring(i, i+1);
+//					k=0;
+					continue;
+				}else if(inputArr[i].equals(hr)){
+					output += "\n--------------------------------------------------------------------------------";
+//					k=80;
+					continue;
 				}
+				
+//				String token = 
+				if(k/80 >= 1){
+					output += "\n";
+				}
+				output += input.substring(i, i+1);
+				k++;
 			}
-			System.out.println(output);
+			
+//		}
 		}
+		
+		
+		System.out.print(output);
 	}
 }
