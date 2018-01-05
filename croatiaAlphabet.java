0e23923045b9db2282ab9123da913bddc796fd64
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-05 17:50:11 +0900

    크로아티아 알파벳 (2941)
    문자열 처리 문제

---
+++ croatiaAlphabet.java
@@ -0,0 +1,33 @@
+package backjoon.BOJ;
+
+import java.util.Scanner;
+
+//2941
+public class croatiaAlphabet {
+	static String[] str = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+
+		boolean find = false;
+		int result =0;
+		for (int i = 0; i < input.length(); i++) {
+			find = false;
+			for (int j = 0; j < str.length; j++) {
+				if(i+str[j].length() <= input.length()){
+					if(str[j].equals(input.substring(i, i+str[j].length()))){
+						result++;
+						i += str[j].length()-1;
+						find = true;
+						break;
+					}
+				}
+			}
+			if(!find){
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
