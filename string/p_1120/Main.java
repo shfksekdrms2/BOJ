cfa691449df03530998087170402cf578a5e7a3a
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-04-12 15:03:18 +0900

    [baekjoon] 1120 문자열
    알고리즘 분류
    (그리디 알고리즘, 브루트 포스, 문자열 처리, 시뮬레이션)

---
+++ string/p_1120/Main.java
@@ -0,0 +1,28 @@
+package backjoon.BOJ.string.p_1120;
+
+import java.util.Scanner;
+
+/*
+ * https://www.acmicpc.net/problem/1120
+ * 문자열
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] a = sc.next().toCharArray();
+		char[] b = sc.next().toCharArray();
+
+		int resultCnt = Integer.MAX_VALUE;
+		for (int i = 0; i <= b.length - a.length; i++) {
+			int cnt = 0;
+			for (int j = i; j < a.length + i; j++) {
+				if (a[j - i] != b[j]) {
+					cnt++;
+				}
+			}
+			resultCnt = Integer.min(resultCnt, cnt);
+		}
+
+		System.out.println(resultCnt);
+	}
+}
