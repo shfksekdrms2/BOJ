32a8620423b7f08bd47325a9da8809a673519e9b
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-06-12 20:45:02 +0900

    [baekjoon] 2583 영역 구하기 - DFS

--- DFS/p_2583/Main.java
+++ DFS/p_2583/Main.java
@@ -1,6 +1,9 @@
 package backjoon.BOJ.DFS.p_2583;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /*
@@ -10,18 +13,16 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int col = sc.nextInt();
 		int row = sc.nextInt();
+		int col = sc.nextInt();
 		int k = sc.nextInt();
 		int[][] arr = new int[row][col];
 		while (k-- > 0) {
-			int startRow = sc.nextInt();
 			int startCol = sc.nextInt();
-			int endRow = sc.nextInt();
+			int startRow = sc.nextInt();
 			int endCol = sc.nextInt();
+			int endRow = sc.nextInt();
 
-			// System.out.println("startRow: " + startRow + " startCol: " +
-			// startCol + " endRow: " + endRow + " endCol: " + endCol);
 			for (int i = startRow; i < endRow; i++) {
 				for (int j = startCol; j < endCol; j++) {
 					arr[i][j] = 1;
@@ -29,23 +30,17 @@ public class Main {
 			}
 		}
 
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println("-----------");
-
 		ArrayList<Integer> list = new ArrayList<>();
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
 				if (arr[i][j] == 0) {
-					list.add(findAreaRange(arr, i, j) + 1);
+					list.add(findAreaRange(arr, i, j));
 				}
 			}
 		}
-
+		Collections.sort(list);
+		
+		System.out.println(list.size());
 		for (Integer integer : list) {
 			System.out.print(integer + " ");
 		}
@@ -56,15 +51,14 @@ public class Main {
 
 	private static int findAreaRange(int[][] arr, int row, int col) {
 
-		System.out.println("row: " + row + " col: " + col);
 		arr[row][col] = 1;
 		int result = 1;
 		for (int i = 0; i < position.length; i++) {
 			int nextRow = row + position[i][0];
 			int nextCol = col + position[i][1];
 			if (!isOutOfArrayIndex(arr, nextRow, nextCol)) {
-				if (arr[nextRow][nextCol] == 1) {
-					result = findAreaRange(arr, nextRow, nextCol) + 1;
+				if (arr[nextRow][nextCol] == 0) {
+					result += findAreaRange(arr, nextRow, nextCol);
 				}
 			}
 		}
