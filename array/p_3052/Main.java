d749784ffb49359629989a34ef81b4e535f1d7dd
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-06-25 20:03:30 +0900

    [baekjoon] 3052 나머지 - Array

---
+++ array/p_3052/Main.java
@@ -0,0 +1,27 @@
+package backjoon.BOJ.array.p_3052;
+
+import java.util.Scanner;
+
+/*
+ * https://www.acmicpc.net/problem/3052
+ * 나머지
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[42];
+		for (int i = 0; i < 10; i++) {
+			int input = sc.nextInt();
+			arr[input % 42]++;
+		}
+
+		int result = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] >= 1) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
