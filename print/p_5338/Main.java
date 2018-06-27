9398c9bc81c263820a3b0e0be2a1c0a32367cb4e
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-06-27 16:49:02 +0900

    [baekjoon] 5338 마이크로소프트 로고 - 출력

---
+++ print/p_5338/Main.java
@@ -0,0 +1,15 @@
+package backjoon.BOJ.print.p_5338;
+
+/*
+ * https://www.acmicpc.net/problem/5338
+ * 마이크로소프트 로고
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n" 
+							+ "\'-..-\'|   ||   |\n" 
+							+ "\'-..-\'|_.-;;-._|\n" 
+							+ "\'-..-\'|   ||   |\n" 
+							+ "\'-..-\'|_.-''-._|");
+	}
+}
