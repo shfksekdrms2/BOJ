351798d05b80d3d38ace9cc5dbc428aa51e25ffb
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-06-29 20:48:31 +0900

    [baekjoon] 10171 고양이 - print

---
+++ print/p_10171/Main.java
@@ -0,0 +1,14 @@
+package backjoon.BOJ.print.p_10171;
+
+/*
+ * https://www.acmicpc.net/problem/10171
+ * 고양이
+ */
+public class Main {
+	public static void main(String[] args){
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
