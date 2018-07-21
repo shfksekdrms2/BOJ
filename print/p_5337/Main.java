f60a66e1b9a4317e4ea5689b2df130788a8d2744
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-07-21 15:41:44 +0900

    [baekjoon] 5337 웰컴 - 출력

---
+++ print/p_5337/Main.java
@@ -0,0 +1,13 @@
+package backjoon.BOJ.print.p_5337;
+
+/*
+ * https://www.acmicpc.net/problem/5337
+ * 웰컴
+ */
+public class Main {
+	public static void main(String[] args){
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
