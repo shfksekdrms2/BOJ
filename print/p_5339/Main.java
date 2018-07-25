95201bd57b8c2e4fcff3aa3dcb09fc4251818c06
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-07-25 10:42:31 +0900

    [baekjoon] 5339 콜센터 - 출력

---
+++ print/p_5339/Main.java
@@ -0,0 +1,20 @@
+package backjoon.BOJ.print.p_5339;
+
+/*
+ * https://www.acmicpc.net/problem/5339
+ * 콜센터
+ */
+public class Main {
+	public static void main(String[] args){
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\ ");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
