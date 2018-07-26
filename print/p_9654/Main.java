da8058cc41e0159403982a0218a39474bc9c35a4
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-07-26 19:18:56 +0900

    [baekjoon] 9654 나부 함대 데이터 - 출력
    String fomat 메소드를 이용하여 좌측 정렬

---
+++ print/p_9654/Main.java
@@ -0,0 +1,16 @@
+package backjoon.BOJ.print.p_9654;
+
+/*
+ * https://www.acmicpc.net/problem/9654
+ * 나부 함대 데이터
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(String.format("%-15s%-15s%-11s%-10s", "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE"));
+		System.out.println(String.format("%-15s%-15s%-11s%-10s", "N2 Bomber", "Heavy Fighter", "Limited", "21"));
+		System.out.println(String.format("%-15s%-15s%-11s%-10s", "J-Type 327", "Light Combat", "Unlimited", "1"));
+		System.out.println(String.format("%-15s%-15s%-11s%-10s", "NX Cruiser", "Medium Fighter", "Limited", "18"));
+		System.out.println(String.format("%-15s%-15s%-11s%-10s", "N1 Starfighter", "Medium Fighter", "Unlimited", "25"));
+		System.out.println(String.format("%-15s%-15s%-11s%-10s", "Royal Cruiser", "Light Combat", "Limited", "4"));
+	}
+}
