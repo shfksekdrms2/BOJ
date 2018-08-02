fb144ff48da44fce16f7e458be8da7d704de4295
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-08-02 22:52:34 +0900

    [baekjoon] 1406 에디터 - 7% 시간 초과

--- LinkedList/p_1406/Main.java
+++ LinkedList/p_1406/Main.java
@@ -1,7 +1,6 @@
 package backjoon.BOJ.LinkedList.p_1406;
 
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -12,44 +11,52 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		char[] arr = sc.next().toCharArray();
-		LinkedList<Character> list = new LinkedList<>();
+		ArrayList<Character> list = new ArrayList<Character>();
 		int testCase = sc.nextInt();
 
 		for (int i = 0; i < arr.length; i++) {
 			list.add(arr[i]);
 		}
-		int index = arr.length;
+		int index = list.size() - 1;
 		for (int i = 0; i < testCase; i++) {
 			String cmd = sc.next();
 			switch (cmd) {
 				case "L":
-					if (index != 0) {
-						index--;
+					if (index > list.size() - 1) {
+						index = list.size() - 1;
 					}
+					index--;
 					break;
 				case "D":
-					if (index != arr.length) {
-						index++;
+					if (index < 0) {
+						index = 0;
 					}
+					index++;
 					break;
 				case "B":
-					if (index != 0) {
+					if (index >= 0) {
 						list.remove(index);
-						index--;
 					}
+					index--;
 					break;
 				case "P":
-					char valuse = sc.next().charAt(0);
-
+					if (index < 0) {
+						index = -1;
+					}
+					index++;
+					if (index > list.size()) {
+						index = list.size();
+					}
+					char value = sc.next().charAt(0);
+					list.add(index, value);
 					break;
 				default:
 					break;
 			}
 		}
-
-		// for (Character character : list) {
-		// System.out.println(character);
-		// }
+		for (Character character : list) {
+			System.out.print(character);
+		}
 
 	}
 }
