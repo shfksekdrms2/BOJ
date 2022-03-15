ab4036939dd798235d7830651acdae3d638d2f27
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2022-03-15 18:23:56 +0900

    10926 문제 해결

---
+++ src/main/java/com/solution/boj/입출력과사칙연산/p_10926/Main.java
@@ -0,0 +1,11 @@
+package com.solution.boj.입출력과사칙연산.p_10926;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String next = sc.next();
+        System.out.println(next + "??!");
+    }
+}
