9fe0f110686983d270e41c2d4398c77d76259950
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2022-11-27 14:57:02 +0900

    클래스명 변경

--- src/main/java/com/solution/boj/p_10951/P_10951.java
+++ src/main/java/com/solution/boj/p_10951/P_10951.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *
  * @Author: jsj0828
  **/
-public class p_10951 {
+public class P_10951 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
