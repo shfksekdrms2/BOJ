f0a8318017e35dbca8ce11f4bc6a9d6a57a0a816
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2022-11-27 14:47:30 +0900

    10951 문제 해결

---
+++ src/main/java/com/solution/boj/p_10951/p_10951.java
@@ -0,0 +1,23 @@
+package com.solution.boj.p_10951;
+
+import java.util.Scanner;
+
+/**
+ * 10951
+ *
+ * @Author: jsj0828
+ **/
+public class p_10951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            String[] split = s.split(" ");
+            int value = Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
+            System.out.println(value);
+        }
+
+    }
+}
